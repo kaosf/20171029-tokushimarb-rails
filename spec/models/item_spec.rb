3b771cbaf33f9161d6d057093c948dea139df34a
ka <ka.kaosf@gmail.com>
2017-10-29 12:52:15 +0900

    Add test cases

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    let(:item) { FactoryBot.build :item }
+    subject { item }
+    it { is_expected.to be_valid }
+    context "name is empty" do
+      before { item.name = '' }
+      it { is_expected.to be_invalid }
+    end
+    context "price is negative" do
+      before { item.price = -1 }
+      it { is_expected.to be_invalid }
+    end
+  end
 end
